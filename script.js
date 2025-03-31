791aaa042eb345558e6d22361ec2ae3fd7ba0bb7
sathvikakotra <sathvikakotra@gmail.com>
2025-03-31 20:23:25 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var phrase = "To be or not to be";
+var age = 25;
+var gpa = 3.1;
+var isMale = false; // Boolean
+var flaws = null;
+var description = undefined;
+
+document.write("<h2 style='color:green;'>Javascript rules!</h2>");
+document.write("<hr/>");
+
+document.write(phrase + "<br>");
+document.write(age + "<br>");
+phrase = "Apple";
+document.write(gpa + "<br>");
+document.write(isMale + "<br>");
+document.write(flaws + "<br>");
+phrase = "Orange";
+document.write(description + "<br>");
+document.write("Length: " + phrase.length + "<br>");
+document.write("Uppercase: " + phrase.toUpperCase() + "<br>");
+document.write("Char at 2: " + phrase.charAt(2) + "<br>");
+document.write("Index of 'r': " + phrase.indexOf("r") + "<br>");
+document.write("Substring (0,3): " + phrase.substring(0, 3) + "<br>");
+document.write("Sum: " + (2 + 2) + "<br>");
+var number = -6;
+document.write ( Math.abs(number) + "<br>");
+document.write ( Math.pow(number, 4) + "<br>");
+document.write ( Math.round(Math.random() * 10) + "<br>");
+// getting user input
+var name = window.prompt ("What is ur name?");
+var age = window.prompt ("How old are you?");
+document.write ( "Hey " + name + ", How are you?" + "<br>");
+document.write ( "Do you like being " + age + " years old ? " + "<br>");
+
+
